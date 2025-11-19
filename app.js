97b240a670adc450f0ce87e626692a3454246ef5
skadudel <skadudelman@gmail.com>
2025-11-19 13:38:31 +0100

    started copy the vid

---
+++ app.js
@@ -0,0 +1,9 @@
+const gameBoard= document.querySelector("#gameboard")
+const playerDisplay= document.querySelector("#player")
+const infoDisplay= document.querySelector("#info-display")
+const width = 8
+const startPieces = [
+    btower, bknight,bbishop,bqueen,bking,bbishop,bknight,btower,
+    bpawn, bpawn, bpawn, bpawn, bpawn, bpawn, bpawn, bpawn,
+    '', '', '', '', '', '', '', '',
+]
---
+++ piece.js
@@ -0,0 +1,12 @@
+const bking = '<div class="piece" id="bking">https://vscode.dev/github/skadudel/Oskar_Schack/blob/main/black_king-removebg-preview.svg</div>'
+const bbishop = '<div class="piece" id="bbishop">https://vscode.dev/github/skadudel/Oskar_Schack/blob/main/black_bishop-removebg-preview.svg</div>'
+const bqueen = '<div class="piece" id="bqueen">https://vscode.dev/github/skadudel/Oskar_Schack/blob/main/black_queen-removebg-preview.svg</div>'
+const bknight = '<div class="piece" id="bknight">https://vscode.dev/github/skadudel/Oskar_Schack/blob/main/black_knight-removebg-preview.svg</div>'
+const bpawn = '<div class="piece" id="bpawn">https://vscode.dev/github/skadudel/Oskar_Schack/blob/main/black_pawn-removebg-preview.svg</div>'
+const btower = '<div class="piece" id="btower">https://vscode.dev/github/skadudel/Oskar_Schack/blob/main/black_tower-removebg-preview.svg</div>'
+
+
+
+
+
+
