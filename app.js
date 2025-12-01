15da8c5c17a6a3de9ada7a7de6dc7d2e7ed4d141
skadudel <skadudelman@gmail.com>
2025-12-01 08:33:41 +0100

    Saker

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const startPieces = [
     '', '', '', '', '', '', '', '',
     '', '', '', '', '', '', '', '',
     '', '', '', '', '', '', '', '',
-    btower, bknight,bbishop,bqueen,bking,bbishop,bknight,btower,
     bpawn, bpawn, bpawn, bpawn, bpawn, bpawn, bpawn, bpawn,
+    btower, bknight,bbishop,bqueen,bking,bbishop,bknight,btower,
 ]
 
 function createBoard(){
@@ -19,7 +19,17 @@ function createBoard(){
         square.classList.add('square')
         square.innerHTML = startPiece
         square.setAttribute('square-id', i)
-        square.classList.add('beige')
+        const row = Math.floor((63 - i) / 8)+1
+        if (row % 2 === 0){
+            square.classList.add(i % 2 === 0 ? "beige":"brown")
+        } else{
+            square.classList.add(i % 2 === 0 ? "brown":"beige")
+        }
+
+        // if ( i <= 15){
+        //     square.firstChild.firstChild.classList.add('black')
+        // }
+
         gameBoard.append(square)
     })
 }
