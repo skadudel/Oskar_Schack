1188cbd394baf3404b856d474ca78081eccae5d0
skadudel <skadudelman@gmail.com>
2025-12-01 21:05:37 +0100

    svg fixes boiiiiiiiiiiiiiiiiiiiiiiiiiiii

--- piece.js
+++ piece.js
@@ -1,9 +1,9 @@
 const bking = '<div class="piece" id="bking"><img src="black_king-removebg-preview.svg"></div>'
-const bbishop = '<div class="piece" id="bbishop"><img src="black_bishop-removebg-preview.svg></div>'
-const bqueen = '<div class="piece" id="bqueen">https://vscode.dev/github/skadudel/Oskar_Schack/blob/main/black_queen-removebg-preview.svg</div>'
-const bknight = '<div class="piece" id="bknight">https://vscode.dev/github/skadudel/Oskar_Schack/blob/main/black_knight-removebg-preview.svg</div>'
-const bpawn = '<div class="piece" id="bpawn">https://vscode.dev/github/skadudel/Oskar_Schack/blob/main/black_pawn-removebg-preview.svg</div>'
-const btower = '<div class="piece" id="btower">https://vscode.dev/github/skadudel/Oskar_Schack/blob/main/black_tower-removebg-preview.svg</div>'
+const bbishop = '<div class="piece" id="bbishop"><img src="black_bishop-removebg-preview.svg"></div>'
+const bqueen = '<div class="piece" id="bqueen"><img src="black_queen-removebg-preview.svg"></div>'
+const bknight = '<div class="piece" id="bknight"><img src="black_knight-removebg-preview.svg"></div>'
+const bpawn = '<div class="piece" id="bpawn"><img src="black_pawn-removebg-preview.svg"></div>'
+const btower = '<div class="piece" id="btower"><img src="black_tower-removebg-preview.svg"></div>'
 
 
 
