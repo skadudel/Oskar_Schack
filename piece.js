185b0a74f36cf1bf23c33cfb80f61d5683e2848d
skadudel <skadudelman@gmail.com>
2025-12-01 08:50:20 +0100

    svg fix hjälp

--- piece.js
+++ piece.js
@@ -1,5 +1,5 @@
-const bking = '<div class="piece" id="bking"><svg xmlns=\"https://vscode.dev/github/skadudel/Oskar_Schack/blob/main/black_king-removebg-preview.svg"/<>/svg></div>'
-const bbishop = '<div class="piece" id="bbishop">https://vscode.dev/github/skadudel/Oskar_Schack/blob/main/black_bishop-removebg-preview.svg</div>'
+const bking = '<div class="piece" id="bking"><img src="black_king-removebg-preview.svg"></div>'
+const bbishop = '<div class="piece" id="bbishop"><img src="black_bishop-removebg-preview.svg></div>'
 const bqueen = '<div class="piece" id="bqueen">https://vscode.dev/github/skadudel/Oskar_Schack/blob/main/black_queen-removebg-preview.svg</div>'
 const bknight = '<div class="piece" id="bknight">https://vscode.dev/github/skadudel/Oskar_Schack/blob/main/black_knight-removebg-preview.svg</div>'
 const bpawn = '<div class="piece" id="bpawn">https://vscode.dev/github/skadudel/Oskar_Schack/blob/main/black_pawn-removebg-preview.svg</div>'
